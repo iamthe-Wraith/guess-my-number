f6eae9809f658380c7e877035f3db069a9864097
iamthe-Wraith <allhailthewraith@protonmail.com>
2022-09-23 18:59:52 -0400

    adds eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  plugins: [
+    '@typescript-eslint',
+    'unused-imports'
+  ],
+  root: true,
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always']
+  },
+};
